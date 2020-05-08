2ce8a93549330f6acee3738fa9f6228f84109380
Arnab <am0072008@gmail.com>
2020-05-08 18:07:44 +0530

    parsing data from client, basic authentication

--- server-master-v1.py
+++ server-master-v1.py
@@ -1,29 +1,66 @@
 import socket
 import sys
 from  multiprocessing import Process
+import json
 
 
 class ServerMaster:
-    
+
     def __init__(self, port, host):
         self.port = port
         self.host = host
 
-    @staticmethod
-    def transform(s):
-        '''Method for some arbitary transform'''
-        s = s.upper()
-        return s
 
-    def client_connect(self):
+    def authenticate(self, user_id, access_key):
+        '''Method for authentication'''
+        db = None
+        with open("chat-db.json") as f:
+            db = json.load(f).get("access_key")
+        if db.get(user_id) == access_key:
+            return True
+        return False
+
+
+    def db_connect(self, to, frm, message):
+        '''Method for db connect'''
+        pass
+
+
+    def command_parse(self,s):
+        '''Method for command parsing
+        input format: {
+            "user_id": "1235",
+            "access_key": "access_key",
+            "message": "message",
+            "to": "1238",
+            "request_dt": '%Y-%m-%d-%H:%M:%S'
+        }
+        '''
+        client_request = json.loads(s)
+        user_id = client_request.get("user_id")
+        access_key = client_request.get("access_key")
+        if not self.authenticate(user_id, access_key):
+            print("authentication error")
+            sys.exit(0)
+        return (
+        client_request.get("to"),
+        client_request.get("user_id"),
+        client_request.get("message")
+        )
+
+
+    def main(self):
+        '''Main method'''
         with socket.socket() as sk:
-            sk.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # reusing port 
+            sk.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # reusing port
             sk.bind((self.host, self.port)) # bind
             sk.listen(5) # listen
-            conn, addrs = sk.accept() # accept 
-            print("serving client:",addrs) 
-            data = conn.recv(1024) # data in
-            print("data received", data)
+            conn, addrs = sk.accept() # accept
+            print("serving client:",addrs)
+            client_request = conn.recv(1024) # data from client
+            to, frm, client_message = self.command_parse(client_request) # command parsing + authentication
+            print("data received", client_message)
+
             conn.close()
             sk.close()
 
@@ -32,6 +69,6 @@ if __name__ == "__main__":
     HOST = "localhost"
     PORT = 8001
     while True:
-        p = Process(ServerMaster(PORT,HOST).client_connect())
+        p = Process(ServerMaster(PORT,HOST).main())
         p.start()
         p.join()
--- test-client.py
+++ test-client.py
@@ -1,18 +1,31 @@
 import socket
 import sys
+import json
+from datetime import datetime
 
-PORT = int(sys.argv[1])
-message = sys.argv[2]
+PORT = 8001
+message = sys.argv[1]
+access_key = "0laph"
+user_id = "1234"
+to = "1267"
 
+client_request = {
+    "user_id": user_id,
+    "access_key": access_key,
+    "message": message,
+    "to": to,
+    "request_dt": datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
+}
 
 with socket.socket() as sk:
-    sk.connect(("localhost", PORT)) # connect 
+    sk.connect(("localhost", PORT)) # connect
 #    while True:
 #        send = input("client>")
 #        sk.sendall(bytes(send.encode("utf-8"))) # data out
 #        if send == "bye": break
 #        data = sk.recv(1024) # data in
-#        print("received:", data) # output in terminal 
-    sk.sendall(bytes(message.encode("utf-8")))
-    
-
+#        print("received:", data) # output in terminal
+    request = bytes(json.dumps(client_request).encode("utf-8"))
+    sk.sendall(request)
+    # data = sk.recv(1024)
+    # print(data)
