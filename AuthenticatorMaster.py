ee5d560ea3c717bd8aaea93160a129a77613f549
Arnab <am0072008@gmail.com>
2020-05-19 23:56:15 +0530

    write draft

--- AuthenticatorMaster.py
+++ AuthenticatorMaster.py
@@ -1,6 +1,20 @@
 import json
 import sys
 from uuid import uuid4
+import sqlite3
+
+class TestDB:
+
+    def __init__(self, database):
+        self.connection = sqlite3.connect(database)
+
+    def db_write(self, column_name, value, table_name, condition):
+        query = """UPDATE {0}
+        SET {1} = '{2}'
+        WHERE {3}""".format(table_name, column_name, value, condition)
+        self.connection.execute(query)
+        self.connection.commit()
+        self.connection.close()
 
 
 class AuthenticatorMaster:
@@ -21,15 +35,18 @@ class AuthenticatorMaster:
 
     def write_access_key(self):
         '''Method for writing acces key and expiry date'''
+        ### move them to config ####
+        db = "./db/access.db"
+        column_name = "access_key"
+        condition_column_name = "user_id"
+        table_name = "access_data"
+        ###############################
         key = self.generate_key()
         user_id = self.user_id
-        temp = None
-        with open("access-db.json") as f:
-            temp = json.load(f)
-            temp[user_id]["access-key"] = key
+        condition = "{0} = '{1}'".format(condition_column_name, user_id)
+        obj = TestDB(db)
+        obj.db_write(column_name, key, table_name, condition)
 
-        with open("access-db.json","w") as f:
-            json.dump(temp, f)
 
     def write_exp_date(self):
         '''Method for writing acces key and expiry date'''
@@ -66,4 +83,4 @@ if __name__ == "__main__":
     access_key = "0laph"
     # print(AuthenticatorMaster(user_id, access_key).authenticate())
     # print(AuthenticatorMaster(user_id, access_key).generate_key())
-    print(AuthenticatorMaster(user_id, access_key).write_access_key())
+    AuthenticatorMaster(user_id, access_key).write_access_key()
