ae52f9152ec34f2cb7aa14cbc2d972dbc74ed66e
shoumikranjan <mukherjee.shoumik@gmail>
2020-05-25 13:16:33 +0530

    exception update v1

--- DB_connector.py
+++ DB_connector.py
@@ -1,29 +1,43 @@
-import sqlite3
-
+from sqlite3 import connect
+from sys import exc_info,exit
 class DB_connector:
 
     def __init__(self,databases):
-        self.conn=sqlite3.connect(databases)
+        self.conn=connect(databases)
 
     def read_from_db(self, column_name, table_name, condition):
         """construct select query for reading data"""
         query= """SELECT {0} from {1}
                   where {2}  """.format(column_name,table_name,condition)
-        cursor=self.conn.execute(query)
-        rows=cursor.fetchall()
-        # self.conn.close()
-        return rows
+
+
+        try:
+            cursor=self.conn.execute(query)
+            rows = cursor.fetchall()
+            return rows
+        except:
+            print(exc_info()[1])
+            print(query)
+            return None
+        #self.conn.close()
+
 
     def insert_message(self,table_name, inserted_row):
         """insert query for writing in the datbase"""
         query= """ insert into {0}
                     values({1})
                 """.format(table_name,inserted_row)
+        try:
+            self.conn.execute(query)
+            self.conn.commit()
+            return 1
+        except:
+            print(exc_info()[1])
+            print(query)
+            return None
 
-        self.conn.execute(query)
-        self.conn.commit()
         # self.conn.close()
-        return True
+
 
     def update_rows(self,table_name,column_name,value,condition):
         """Create a query to update row/rows(record/records) in table"""
@@ -35,26 +49,48 @@ class DB_connector:
             query = """UPDATE {0}
                             SET {1} = {2}
                             WHERE {3}""".format(table_name, column_name, value, condition)
-        self.conn.execute(query)
-        self.conn.commit()
+        try:
+            self.conn.execute(query)
+            self.conn.commit()
+            return 1
+        except:
+            print(exc_info()[1])
+            print(query)
+            return None
         # self.conn.close()
-        return True
+
 
     def delete_message_DB(self,table_name, condition):
         """Creates a query which deletes record/records"""
         query = """DELETE FROM {0}
                         WHERE {1}""".format(table_name, condition)
-        self.conn.execute(query)
-        self.conn.commit()
+        try:
+            self.conn.execute(query)
+            self.conn.commit()
+            return 1
+        except:
+            print(exc_info()[1])
+            print(query)
         # self.conn.close()
-        return True
+
 
     def close_db(self):
         '''Method for connection closure'''
-        self.conn.close()
+        try:
+            self.conn.close()
+        except:
+            print(exc_info()[1])
+            exit(0)
         return 1
 
 
 if __name__ == "__main__":
     # Add uinit test cases
+    #db = 'G:\Git_Code\chat-app\db\mydb.db'
+    #inserted_message="1234,2548,'2020-05-19','N','Hello'"
+    #DB_connector(db).insert_message('message_table',inserted_message)
+    #DB_connector(db).delete_message_DB('message_table','abc')
+    #DB_connector(db).update_rows('message_table','message','def', 'abc')
+    #print(DB_connector(db).read_from_db('messag','message_table','sender=1234 and receiver= 2548'))
     pass
+
--- message_handling.py
+++ message_handling.py
@@ -16,9 +16,10 @@ class Message_handling:
         table_name='message_table'
         condition= "sender='{0}' and receiver='{1}' and read_flag='N'".format(self.to_user_id,self.from_user_id)
         obj=DB_connector(db)
-        rows=obj.Read_from_DB(column_name,table_name,condition)
+        rows=obj.read_from_db(column_name,table_name,condition)
         column_name='read_flag'  #for updating read_flag
         obj.update_rows(table_name,column_name,'Y',condition) #for updating read_flag
+        obj.close_db()
         return (rows)
 
     def write_message(self):
@@ -27,8 +28,9 @@ class Message_handling:
         table_name = 'message_table'
         inserted_row ="{0},{1},'2020-05-19','N','{2}'".format(self.to_user_id,self.from_user_id,self.message_Write)
         obj = DB_connector(db)
-        return obj.insert_message(table_name,inserted_row)
-
+        obj.insert_message(table_name,inserted_row)
+        obj.close_db()
+        return 1
 
 
 
@@ -38,14 +40,15 @@ class Message_handling:
         table_name = 'message_table'
         condition = "read_flag='Y'"
         obj = DB_connector(db)
-        return obj.delete_message_DB(table_name,condition)
-
+        obj.delete_message_DB(table_name,condition)
+        obj.close_db()
+        return 1
 
 
 if __name__ == "__main__":
     message1=Message_handling(1254, 2548)
     #print(message1.write_message())
-    #print(message1.read_message())
+    print(message1.read_message())
     #Message_handling(1254,2548,'Hello Lovebird!!').write_message()
-    print(message1.delete_message())
+    #print(message1.delete_message())
 #input("please enter user id"),input("please enter user id")
